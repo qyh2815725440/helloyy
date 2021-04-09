64419a5edfefd7eca1fba7b0922cc2f53fe978b7
qyh2815725440 <2815725440@qq.com>
2021-04-09 11:01:41 +0800

    version2.2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("lbwnb6666");
-        System.out.println("nb我的宝贝");
+        System.out.println("我的宝贝");
     }
 }
